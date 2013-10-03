e88da42065cfc52e7f91d3e87530741de0ce41aa
zywy <m.zywno@gmail.com>
2013-10-03 22:40:19 +0200

    add missing 'it' block

--- spec/models/calculator_spec.rb
+++ spec/models/calculator_spec.rb
@@ -16,16 +16,19 @@ describe CCalculator do
   end
 
   describe '.male_bee' do
-    it 'returns correct male bee value' do
+    it 'returns correct value' do
       calculator = CCalculator.new
-      bee =  calculator.send(:male_bee, 60, 198)
+      bee =  calculator.send(:male_bee, 60, 198, 0)
+      bee.should == 0
     end
   end
 
 
   describe '.female_bee' do
+    it 'returns correct value' do
       calculator = CCalculator.new
       bee = calculator.send(:female_bee, 11.2, 12.3, 15)
       bee.should == 0
+    end
   end
 end
