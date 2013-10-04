42c6d11e01aebe50bd3346ba08e41cef635923cd
zywy <m.zywno@gmail.com>
2013-10-04 13:21:31 +0200

    format

--- spec/requests/calories_form_spec.rb
+++ spec/requests/calories_form_spec.rb
@@ -3,37 +3,37 @@ require "spec_helper"
 feature 'calories form' do
 	# happy path
 	context 'when a user fill calories form with all data' do
-	    background do
-	      visit static_pages_calories_form_path
-	    end
-		scenario "calculate calories" do
-	 	    within "form" do
-	 	    	fill_in "age", with: "20"
-		        fill_in "weight", with: "90"
-		        fill_in "height", with: "190"
-		        
-		        click_button "Dajesz kalorie"
-      	    end
-            page.should display_flash_message("Obliczylismy twoje kalorie. ponizej rezultat")
-            page.should have_content("2134.43") # obliczony rezultat
-		end
+    background do
+      visit static_pages_calories_form_path
+    end
+    scenario "calculate calories" do
+      within "form" do
+        fill_in "age", with: "20"
+          fill_in "weight", with: "90"
+          fill_in "height", with: "190"
+
+          click_button "Dajesz kalorie"
+        end
+        page.should display_flash_message("Obliczylismy twoje kalorie. ponizej rezultat")
+        page.should have_content("2134.43") # obliczony rezultat
+    end
 	end
 
 	# fail path
 	context 'when a user fill calories form with NOT all required data' do
-	    background do
-	      visit static_pages_calories_form_path
-	    end
+    background do
+      visit static_pages_calories_form_path
+    end
 		scenario "show error message" do
-	 	    within "form" do
-		        fill_in "weight", with: "" # missing weight
-		        fill_in "height", with: "190"
-		        fill_in "age", with: "20"
-		        click_button "Dajesz kalorie"
-      	    end
-            page.should have_content("podaj wage") # a tu nie lepiej sprawdzic czy jest jakis error flash message?
-            # tak, mozna tez sprawdzic ze jest klasa css .error czy .error-message nie pameitam
-            # i ze dany message jest przy danym polu (weight) - to musisz doczytac :D - spoko:)
+      within "form" do
+        fill_in "weight", with: "" # missing weight
+        fill_in "height", with: "190"
+        fill_in "age", with: "20"
+        click_button "Dajesz kalorie"
+      end
+      page.should have_content("podaj wage") # a tu nie lepiej sprawdzic czy jest jakis error flash message?
+      # tak, mozna tez sprawdzic ze jest klasa css .error czy .error-message nie pameitam
+      # i ze dany message jest przy danym polu (weight) - to musisz doczytac :D - spoko:)
 		end
 	end
 end
