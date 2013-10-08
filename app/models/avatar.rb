126904ac58a6098f0cbc7d362a92cda60094fbbb
wolak88 <wolak88@gmail.com>
2013-10-08 13:52:28 +0200

    Added few more tests

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -2,7 +2,9 @@ class Avatar < ActiveRecord::Base
   validates :age, presence: true
   validates :age, numericality: { greater_than: 14, less_than: 100 }
   validates :height, presence: true
+  validates :height, numericality: { greater_than: 130, less_than: 230 }
   validates :weight, presence: true
+  validates :weight, numericality: { greater_than: 40, less_than: 160 }
   validates :male, presence: true
   validates :activeness, presence: true
 end
