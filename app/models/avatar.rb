ab324cf88fd8ad6f8d443e4f65aafb7f0e591e48
wolak88 <wolak88@gmail.com>
2013-10-08 13:28:52 +0200

    Added age validation

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,5 +1,6 @@
 class Avatar < ActiveRecord::Base
   validates :age, presence: true
+  validates :age, numericality: { greater_than: 14, less_than: 100 }
   validates :height, presence: true
   validates :weight, presence: true
   validates :male, presence: true
