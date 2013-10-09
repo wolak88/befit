5d609d1eed6f48443cc35845b7f0125e2773f62a
wolak88 <wolak88@gmail.com>
2013-10-09 11:40:03 +0200

    Deleted duplicated test

--- spec/features/avatar_manage_spec.rb
+++ spec/features/avatar_manage_spec.rb
@@ -43,18 +43,7 @@ feature 'avatar form' do
 
     page.should have_content("Weight must be less than 160")
   end
-
-  scenario 'User fills age not from range 14-99' do
-    within "form" do
-      fill_in "Weight", with: "94"
-      fill_in "Height", with: "190"
-      fill_in "Age", with: "110"
-      click_button "Dajesz kalorie"
-    end
-
-    page.should have_content("Wiek musi być w przedziale 14-99")
-  end
-
+  
 end
 
 feature 'Avatar show' do
